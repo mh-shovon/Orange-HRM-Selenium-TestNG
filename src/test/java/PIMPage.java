cb46e812976d490106df48a2dfa359d7c4fd20d7
mh-shovon <shovon33266@gmail.com>
2024-04-29 16:18:02 +0600

    Automate Edit Employee functionality with assertion.

--- src/test/java/PIMPage.java
+++ src/test/java/PIMPage.java
@@ -22,8 +22,22 @@ public class PIMPage {
     @FindBy(className = "oxd-switch-input")
     WebElement toggleButton;
 
-    @FindBy(className = "oxd-button")
+    @FindBy(className = "oxd-input")
     List<WebElement> txtFields;
+
+    @FindBy(className = "oxd-topbar-body-nav-tab-item")
+    List<WebElement> employeeListTab;
+
+    @FindBy(xpath = "//div[@class='oxd-grid-4 orangehrm-full-width-grid']//div[1]//div[1]//div[2]//div[1]//div[1]//input[1]")
+    WebElement txtEmployeeName;
+
+    @FindBy(className = "oxd-icon-button")
+    List<WebElement> iconButtons;
+
+    @FindBy(className = "oxd-radio-input")
+    List<WebElement> radioButtons;
+
+
 
     public PIMPage(WebDriver driver) {
         PageFactory.initElements(driver,this);
@@ -40,6 +54,25 @@ public class PIMPage {
         txtFields.get(6).sendKeys(passWord); // Insert Password
         txtFields.get(7).sendKeys(passWord); //Insert Confirm Password
         button.get(1).click(); //Click on Save button
+    }
+
+    public void editEmployee(String employeeName, String employeeOtherId) throws InterruptedException {
+        pimButton.get(1).click(); //Select PIM tab from Menu bar
+        Thread.sleep(3000);
+        employeeListTab.get(1).click();
+        Thread.sleep(500);
+        txtEmployeeName.sendKeys(employeeName);
+        Thread.sleep(500);
+        button.get(1).click();
+        Thread.sleep(500);
+        iconButtons.get(4).click();
+        Thread.sleep(500);
+        txtFields.get(5).sendKeys(employeeOtherId);
+        Thread.sleep(500);
+        radioButtons.get(0).click();
+        Thread.sleep(500);
+        button.get(0).click();
+        Thread.sleep(500);
     }
 }
 
--- src/test/java/PIMTestRunner.java
+++ src/test/java/PIMTestRunner.java
@@ -21,13 +21,26 @@ public class PIMTestRunner extends Setup {
     @Test(priority = 1)
     public void addEmployee() throws InterruptedException {
         PIMPage pimPage = new PIMPage(driver);
-        pimPage.addEmployee("Hasan", "Shovon", "hasanshovon6667", "P@ssword123");
-        List<WebElement> headerElement = driver.findElements(By.className("orangehrm-main-title"));
-        String textActual = headerElement.get(0).getText();
+        Thread.sleep(1000);
+        pimPage.addEmployee("Hasan", "Shovon", "hasanshovon666700", "P@ssword123");
+        WebElement headerElement = driver.findElement(By.xpath("//h6[normalize-space()='Personal Details']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        wait.until(ExpectedConditions.visibilityOf(headerElement));
+        String textActual = headerElement.getText();
         String textExpected = "Personal Details";
-        Thread.sleep(2000);
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-//        wait.until(ExpectedConditions.visibilityOf((WebElement) headerElement));
         Assert.assertEquals(textActual, textExpected);
+    }
+
+    @Test(priority = 2)
+    public void editEmployee() throws InterruptedException {
+        PIMPage pimPage = new PIMPage(driver);
+        pimPage.editEmployee("Hasan Shovon", "666700");
+        WebElement headerElement = driver.findElement(By.xpath("//h6[normalize-space()='Personal Details']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        wait.until(ExpectedConditions.visibilityOf(headerElement));
+        String textActual = headerElement.getText();
+        String textExpected = "Personal Details";
+        Assert.assertEquals(textActual, textExpected);
+
     }
 }
